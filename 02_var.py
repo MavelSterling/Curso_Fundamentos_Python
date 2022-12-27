d079b260d2ac34e8ed76cccf3eeb43cf8ad56146
MavelSterling <mavelynsl@gmail.com>
2022-12-27 01:26:11 -0500

    Create 02_var.py

---
+++ 02_var.py
@@ -0,0 +1,20 @@
+print("Hola, vars")
+
+my_name = "name"
+print(my_name)
+
+# esto es variable con un numero
+my_age = 12
+print(my_age)
+
+my_name = "last name"
+print("aqui cambio", my_name)
+
+# input
+my_name = input("¿Cuál es tu nombre?")
+print("usando input", my_name)
+
+
+# input
+my_age = input("¿Cuál es tu edad?")
+print("usando input", my_age)
