8fb7b8efd45757b0328f6c169c20538f7a1e61f8
MavelSterling <mavelynsl@gmail.com>
2022-12-27 01:42:02 -0500

    Create 03_types.py

---
+++ 03_types.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+
+# string
+my_name = "Nombre 1"
+my_name = 'Nombre 2'
+my_name = "12"
+print('my_name =>', my_name)
+print(type(my_name))
+
+# int
+my_age = 12
+print('my_age =>', my_age)
+print(type(my_age))
+
+# boolean
+is_single = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('¿Cuál es tu edad? ')
+print('my_age =>', my_age)
+print(type(my_age))
