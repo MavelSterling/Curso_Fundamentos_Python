0a1562b7e5972d964fefb31003673c7879ebc759
MavelSterling <mavelynsl@gmail.com>
2023-02-03 01:26:03 -0500

    Create 05_boolean.py

---
+++ 05_boolean.py
@@ -0,0 +1,13 @@
+is_single = True
+print(type(is_single))
+
+is_single = False
+print(is_single)
+
+print(not True)
+print(not False)
+
+# Cambiar el ultimo valor de is_single
+is_single= not is_single
+print(is_single)
+
