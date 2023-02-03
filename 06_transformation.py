6011f7f9a6b15b00d7a80bf3b8c5f9e904284d57
MavelSterling <mavelynsl@gmail.com>
2023-02-03 01:36:59 -0500

    Update 06_transformation.py

--- 06_transformation.py
+++ 06_transformation.py
@@ -9,12 +9,13 @@ print("Nicolas" + " Molina")
 print(10 + 20)
 print("Nicolas" + "12")
 
+
 age = 12
 print("Mi edad es " + str(age))
 print(f"Mi edad es {age}")
 
-age = input('Escribe tu edad => ')
+age = input('Escribe tu edad => ' )
 print(type(age))
 age = int(age)
 age += 10
-print(f'Tu edad en 10 aÃ±os será¡ {age}')
+print(f'Tu edad en 10 años será {age}')
