4ed455e948c1dcf43ea3878a92a36addac8c8e6b
MavelSterling <mavelynsl@gmail.com>
2023-02-07 00:22:13 -0500

    Update 08_comparators.py

--- 08_comparators.py
+++ 08_comparators.py
@@ -1,36 +1,36 @@
 # > 
-print(7 > 3)
-print(3 > 7)
-print(7 > 7)
+print("7 > 3 ?: ",7 > 3)
+print("3 > 7 ?: ",3 > 7)
+print("7 > 7 ?: ",7 > 7)
 
 # <
-print(5 < 6)
-print(6 < 5)
-print(5 < 5)
+print("5 < 6 ?: ",5 < 6)
+print("6 < 5 ?: ",6 < 5)
+print("5 < 5 ?: ",5 < 5)
 
 # >=
-print(2 >= 1)
-print(2 >= 3)
-print(2 >= 2)
+print("2 >= 1 ?: ", 2 >= 1)
+print("2 >= 3 ?: ",2 >= 3)
+print("2 >= 2 ?: ",2 >= 2)
 
 # <=
-print(1 <= 2)
-print(2 <= 1)
-print(2 <= 2)
+print("1 <= 2 ?: ", 1 <= 2)
+print("2 <= 1 ?: ", 2 <= 1)
+print("2 <= 2 ?: ", 2 <= 2)
 
 # ==
 
-print(6 == 6)
-print(5 == 2)
+print("6 es igual a 6 ?:", 6 == 6)
+print("5 es igual a 2 ?:", 5 == 2)
 
 # Distinto !=
 
-print(6 != 10)
-print(6 != 6)
+print("6 es distinto a 10 ?: ", 6 != 10)
+print("6 es distinto a 6 ?: ",6 != 6)
 
-print("Apple" == 'Apple')
-print("Apple" == 'apple')
-print("1" == 1)
+print("Apple" == 'Apple') # true 
+print("Apple" == 'apple') # false
+print("'1' == 1 ?: ","1" == 1)
 
 age = 15
-print(age >= 18)
+print("15 >= 18 ?: ",age >= 18)
