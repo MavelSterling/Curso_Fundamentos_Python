0d5f97f61a37128c9dbd0cefb134133739f0b393
MavelSterling <mavelynsl@gmail.com>
2023-02-06 23:50:09 -0500

    Update 07_arith.py

--- 07_arith.py
+++ 07_arith.py
@@ -1,20 +1,19 @@
 
-print(10 + 10)
-print(10 - 5)
-print(10 * 2)
-print(10 / 2) # 5
-print(10 % 2) # 0
-print(10 / 3)
-print(round(10 / 3 ,2))
-print(10 % 3)
-print(10 // 3)
+print(10 + 10) # suma
+print(10 - 5) # resta
+print(10 * 2) # multiplicacion
+print(10 / 2) # 5  - Division
+print(10 % 2) # 0 - Módulo o residuo
+print(10 / 3) # Division 
+print(round(10 / 3 ,2)) # redondear decimales
+print(10 % 3) 
+print(10 // 3) # division con valores enteros 
+print(2 ** 3) # Exponencial 
+print(2 ** 3 + 3 - 7 / 1 // 4) # jerarquia
 print(2 ** 3)
-print(2 ** 3 + 3 - 7 / 1 // 4)
-print(2 ** 3)
-print((7 / 1) // 4)
+print((7 / 1) // 4) 
 
 print(8 + 3 - 1)
-print(10 / 0)
 
 print('Hola' + ' mundo')
 print('Hola' * 3)
