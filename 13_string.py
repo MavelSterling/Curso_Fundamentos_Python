4010bca497e9c2aaab502620e83da8486553fecb
MavelSterling <mavelynsl@gmail.com>
2023-02-10 01:58:27 -0500

    Update 13_string.py

--- 13_string.py
+++ 13_string.py
@@ -12,21 +12,21 @@ else:
 '''
 
 size = len(text)
-print(size)
+print("El tamaño de: ", text, ". Es :", size)
 
-print(text)
-print(text.upper())
-print(text.lower())
-print(text.count('a'))
+print("El mensaje es: ", text)
+print("Mensaje en mayúscula: ", text.upper())
+print("Mensaje minúscula: ", text.lower())
+print("Contar la letra a en el mensaje es:  ", text.count('a'))
 
-print(text.swapcase())
-print(text.startswith('Ella'))
-print(text.endswith('Rust'))
-print(text.replace('Python', 'Go'))
+print("Invierte las minúsculas o mayúsculas del mensaje original: ", text.swapcase())
+print("El mensaje original inicia con 'Ella': ",text.startswith('Ella'))
+print("El mensaje original termina con 'Rust': ",text.endswith('Rust'))
+print("Reemplaza la palabra 'Python' por 'Go' en el mensaje original: ",text.replace('Python', 'Go'))
 
 text_2 = 'este es un titulo'
 print(text_2)
 print(text_2.capitalize())
 print(text_2.title())
-print(text_2.isdigit())
-print("398".isdigit())
+print(text_2,". Es un digito? " ,text_2.isdigit())
+print("'398' es un digito? ","398".isdigit())
