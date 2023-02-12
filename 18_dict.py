c4fbb719d86caab00c401a6c44a40a7f38096354
MavelSterling <mavelynsl@gmail.com>
2023-02-12 15:50:33 -0500

    Update 18_dict.py

--- 18_dict.py
+++ 18_dict.py
@@ -3,7 +3,7 @@
 # { } = Diccionarios
 
 my_dict = {}
-print(type(my_dict))
+print("El tipo de la variable my_dict es: ",type(my_dict))
 
 my_dict = {
   'avion': "bla bla bla",
@@ -12,11 +12,15 @@ my_dict = {
   'age': 87
 }
 
-print(my_dict)
-print(len(my_dict))
+print("El diccionario es: ",my_dict)
+print("La cantidad items en el diccionario es: " ,len(my_dict))
 
-print(my_dict['age'])
-print(my_dict['last_name'])
+print("La edad almacena en el diccionario es: " , my_dict['age'])
+print("El apellido almacena en el diccionario es: " + my_dict['last_name'])
+
+
+#La funcion get, si no existe el valor sale un mensaje none(Maneja el error)
+# Se cambian los corchetes por parentesis(), ya que get es una funcion de python
 print(my_dict.get('age'))
 
 print('avion' in my_dict)
