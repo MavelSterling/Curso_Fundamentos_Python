fc8290bcd327168939896cabcb61ebdb2aeec8f4
MavelSterling <mavelynsl@gmail.com>
2023-02-12 16:06:08 -0500

    Update 19_dict.py

--- 19_dict.py
+++ 19_dict.py
@@ -25,3 +25,69 @@ print(person.keys())
 
 print('values')
 print(person.values())
+
+
+Harry_potter={
+    "name": "Harry Potter",
+    "species": "human",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "31-07-1980",
+    "yearOfBirth": 1980,
+    "ancestry": "half-blood",
+    "eyeColour": "green",
+    "hairColour": "black",
+    "wand": {
+      "wood": "holly",
+      "core": "phoenix feather",
+      "length": 11
+    },
+    "patronus": ["stag",'Fox'],
+    "hogwartsStudent": True,
+    "hogwartsStaff": False,
+    "actor": "Daniel Radcliffe",
+    "alive": True,
+    "image": "https://hp-api.herokuapp.com/images/harry.jpg"
+  }
+
+#Agregando un dato a un diccionario que esta en un diccionario ppal
+print(Harry_potter['wand']['core'])
+DATA2['wand']['core']+= ' dragon heartstring'
+print(Harry_potter['wand']['core'])
+
+#agregando un valor a una lista que esta en un diccionario ppal
+print(Harry_potter['patronus'])
+Harry_potter["patronus"].append('cat')
+print(Harry_potter['patronus'])
+
+#eliminar un par clave, valor de una lista que esta en un diccionario ppal
+del Harry_potter["actor"]
+Harry_potter.pop('gender')
+print(Harry_potter)
+
+#Obtener items de un diccionario
+print('')
+print('Items')
+print(Harry_potter.items())
+
+#Imprime las keys del diccionario
+print('')
+print('keys')
+print(Harry_potter.keys())
+
+
+#imprime las keys del diccionar que esta en un diccionar ppal
+print('')
+print('keys of dict in dict')
+print(Harry_potter["wand"].keys())
+
+#Imprime los values  del diccionario
+print('')
+print('values')
+print(Harry_potter.values())
+
+
+#imprime las keys del diccionar que esta en un diccionar ppal
+print('')
+print('values of dict in dict')
+print(Harry_potter["wand"].values())
