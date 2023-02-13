a65d6ebd84e0f2c6cdf6876c46aa148e19e7290b
MavelSterling <mavelynsl@gmail.com>
2023-02-13 01:01:25 -0500

    Create exam.py

---
+++ exam.py
@@ -0,0 +1,25 @@
+result=990
+if result >= 1000:
+    print("El resultado es myaor o igual a 1000")
+else:
+    print("El resultado es menor a 1000")
+    
+print(8000 > 3330)
+
+city = 'NY'
+if city == 'NY':
+    print("welcome to New York City")
+    
+name ='Mario Salvador'
+name = name.replace('Mario', 'Juan')
+print(name)
+
+print(int(4.56))
+
+print('Hola,' + " " + 'Platzinauta')
+
+print(False or True)
+
+print((8 / 2) + 4 * 8)
+
+print( 4 != 10)
