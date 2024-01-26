1bbd49353f62357527e494f237ccd6e6913de201
MavelSterling <mavelynsl@gmail.com>
2024-01-25 23:15:46 -0500

    Create funcion.py

---
+++ PI/Sintaxis/funcion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# def nombre_función(parámetros):
+#     instrucciones de la función
+#     return (opcional) -> tiene que ver con devolver valores
+
+# Se ejecuta: nombre_funcion() o nombre_función(parámetros)
+
+def mensaje(): # <- declaraciónde la función
+    print("Estamos aprendiendo Python")
+    print("Estamos aprendiendo instrucciones básicas")
+
+mensaje() # <- llamada a la función
+
+
